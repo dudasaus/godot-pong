58f335a38e3fd7ceef9afe672e3b79040110f6d4
Austin Dudas <dudasaus@umich.edu>
2024-09-22 17:44:15 -0600

    Keep paddles in bounds

--- paddle.gd
+++ paddle.gd
@@ -3,6 +3,12 @@ extends StaticBody2D
 
 const SPEED = 300.0
 var velocity = Vector2.ZERO
+var height = 120.0
+var min_pos_y = 0.0 + height / 2.0
+var max_pos_y
+
+func _ready() -> void:
+	max_pos_y = get_viewport_rect().size.y - height / 2.0
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_up", "ui_down")
@@ -12,3 +18,4 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
 
 	position += velocity * delta
+	position.y = clamp(position.y, min_pos_y, max_pos_y)
